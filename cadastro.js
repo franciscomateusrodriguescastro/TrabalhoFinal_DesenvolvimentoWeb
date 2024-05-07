8b0602dfe74e412559c0cef6b233a62c65c32295
franciscomateusrodriguescastro <matesrodrigues@gmail.com>
2024-05-07 09:08:30 -0300

    Atualização

--- cadastro.js
+++ cadastro.js
@@ -1,18 +1,17 @@
-document.getElementById('cadastroForm').addEventListener('submit', async function(event) {
+document.getElementById('registerForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+    const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     try {
-        const response = await axios.post('http://exemplo.com/api/usuarios', { name, email, password });
+        const response = await axios.post('http://exemplo.com/api/register', { username, password });
         console.log(response.data);
         alert('Usuário cadastrado com sucesso!');
         // Redirecionar para a página de login após o cadastro
         window.location.href = 'login.html';
     } catch (error) {
-        console.error('Erro ao cadastrar o usuário:', error);
-        alert('Erro ao cadastrar o usuário.');
+        console.error('Erro ao cadastrar usuário:', error);
+        alert('Erro ao cadastrar usuário. Tente novamente mais tarde.');
     }
 });
