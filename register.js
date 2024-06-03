3d9956503c487015d8346b9ea4c3d9d1877e7509
franciscomateusrodriguescastro <matesrodrigues@gmail.com>
2024-06-03 20:19:34 -0300

    atualização

--- register.js
+++ register.js
@@ -14,9 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         try {
-            const response = await axios.post('URL_DA_SUA_API', data);
-            alert('Usuário cadastrado com sucesso!');
-            registerForm.reset();
+            const response = await axios.post('http://localhomongodb://127.0.0.1:27017/clientesCollection?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6st:3000/api/register', data);
+            if (response.status === 200) {
+                alert('Usuário cadastrado com sucesso!');
+                registerForm.reset();
+            } else {
+                alert('Erro ao cadastrar usuário. Tente novamente mais tarde.');
+            }
         } catch (error) {
             console.error('Erro ao cadastrar usuário:', error);
             alert('Erro ao carregar usuários. Tente novamente mais tarde.');
