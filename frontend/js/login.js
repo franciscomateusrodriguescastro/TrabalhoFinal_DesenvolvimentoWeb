d073f382126d0d517e8606cdbca316a14e7e3ef4
franciscomateusrodriguescastro <150175920+franciscomateusrodriguescastro@users.noreply.github.com>
2024-06-07 16:33:36 -0300

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     const password = document.getElementById('password').value;
 
     try {
-        const response = await axios.post('mongodb+srv://mateus:dbmateus99@cluster0.1mwlztn.mongodb.net/', { email, password });
+        const response = await axios.post('mongodb+srv://mateusdb:dbmateus99@cluster0.1mwlztn.mongodb.net/', { email, password });
         localStorage.setItem('token', response.data.token);
         window.location.href = 'admin.html';
     } catch (error) {
